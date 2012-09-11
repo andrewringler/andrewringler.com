66ca1fdceaee4a4f07d9885e6d6fe6b2cbde0420
Andrew Ringler <ringler@gmail.com>
2012-09-10 20:14:54 -0400

    rolled back sidebar change, that didn't work, think i'll have to do a custom grid here, don't like the onecol, twocol business, I don't think, we'll wait on this

--- andrewringler-wp-theme/home.php
+++ andrewringler-wp-theme/home.php
@@ -4,7 +4,7 @@
 			
 				<div id="inner-content" class="wrap clearfix">
 			
-				    <div id="main" class="sevencol first clearfix" role="main">
+				    <div id="main" class="eightcol first clearfix" role="main">
 
               <?php query_posts( 'posts_per_page=1' ); ?>
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
