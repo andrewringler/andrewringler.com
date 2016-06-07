203261319db31445f8b2a47be4acfc10c6da5289
Andrew Ringler <ringler@gmail.com>
2016-06-07 17:51:00 -0400

    added include of ACF to theme

--- andrewringler-wp-theme/functions.php
+++ andrewringler-wp-theme/functions.php
@@ -191,25 +191,25 @@ require get_template_directory() . '/inc/jetpack.php';
  * https://www.advancedcustomfields.com
  * https://www.advancedcustomfields.com/resources/including-acf-in-a-plugin-theme/
  */
-//  // 1. customize ACF path
-// add_filter('acf/settings/path', 'my_acf_settings_path');
-// function my_acf_settings_path( $path ) { 
-//     $path = get_stylesheet_directory() . '/acf/';    
-//     return $path;    
-// }
-//  
-// // 2. customize ACF dir
-// add_filter('acf/settings/dir', 'my_acf_settings_dir');
-// function my_acf_settings_dir( $dir ) {
-//     $dir = get_stylesheet_directory_uri() . '/acf/';    
-//     return $dir;    
-// }
-//  
-// // 3. Hide ACF field group menu item
-// add_filter('acf/settings/show_admin', '__return_false');
-// 
-// // 4. Include ACF
-// include_once( get_stylesheet_directory() . '/acf/acf.php' );
+ // 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+function my_acf_settings_path( $path ) { 
+    $path = get_stylesheet_directory() . '/acf/';    
+    return $path;    
+}
+ 
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+function my_acf_settings_dir( $dir ) {
+    $dir = get_stylesheet_directory_uri() . '/acf/';    
+    return $dir;    
+}
+ 
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_false');
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
 
  
 if(function_exists("register_field_group"))
